49a5774aa65bce7cc9974ee473715df2dbd49ebe
erozonachi <erozonachi.james@gmail.com>
2019-07-02 11:34:32 +0100

    Create and export action types

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const START = 'START';
+export const SHOLVING = 'SHOLVING';
+export const PLAY_MODE = 'PLAY_MODE';
+export const PLAY = 'PLAY';
+export const HOLD_ON = 'HOLD_ON';
+export const GO_MARKET = 'GO_MARKET';
+export const PICK_TWO = 'PICK_TWO';
+export const LAST_CARD = 'LAST_CARD';
+export const FINISH = 'FINISH';
+export const WIN = 'WIN';
+export const LOSS = 'LOSS';
