e18b52c9e58f8424d951a9eea5a25ba40bb61dec
erozonachi <erozonachi.james@gmail.com>
2019-07-02 19:16:13 +0100

    Create and export triangle styled component

--- src/components/cards/StyledComponents/index.js
+++ src/components/cards/StyledComponents/index.js
@@ -5,3 +5,8 @@ export const Circle = styled.div`
   clip-path: circle(50% at 50% 50%);
   width: ${props.small? '30px' : '80%'};
 `;
+
+export const Triangle = styled.div`
+  -webkit-clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+  clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+`;
