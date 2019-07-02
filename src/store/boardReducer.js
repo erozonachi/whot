dac95be2ba6fd061099ed9914f0229f9f89404f7
erozonachi <erozonachi.james@gmail.com>
2019-07-02 12:29:13 +0100

    Create and export board reducer

---
+++ src/store/boardReducer.js
@@ -0,0 +1,53 @@
+import * as types from '../actions';
+
+const initialState = {
+  playersCards: {},
+  marketArea: [],
+  playArea: [],
+};
+
+export function boardReducer(state = initialState, action) {
+  switch(action.type) {
+    case types.START:
+      return {
+        ...initialState,
+      };
+    case types.SHOLVING:
+      return {
+        ...state,
+        marketArea: action.payload.marketArea,
+        playersCards: action.payload.playersCards,
+      };
+    case types.PLAY:
+      return {
+        ...state,
+        playersCards: {
+          ...state.playersCards,
+          ...action.playersCards,
+        },
+        marketArea: action.marketArea,
+      };
+    case types.GO_MARKET:
+      return {
+        ...state,
+        playersCards: {
+          ...state.playersCards,
+          ...action.playersCards,
+        },
+        marketArea: action.marketArea,
+      };
+    case types.PICK_TWO:
+      return {
+        ...state,
+        playersCards: {
+          ...state.playersCards,
+          ...action.playersCards,
+        },
+        marketArea: action.marketArea,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+}
