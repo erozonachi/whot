a5d1fa0ce76174a2be2fd4bff9449a9f3862a703
erozonachi <erozonachi.james@gmail.com>
2019-07-02 12:30:36 +0100

    Add and export general_market action type

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ export const PLAY_MODE = 'PLAY_MODE';
 export const PLAY = 'PLAY';
 export const HOLD_ON = 'HOLD_ON';
 export const GO_MARKET = 'GO_MARKET';
+export const GENERAL_MARKET = 'GENERAL_MARKET';
 export const PICK_TWO = 'PICK_TWO';
 export const LAST_CARD = 'LAST_CARD';
 export const FINISH = 'FINISH';
