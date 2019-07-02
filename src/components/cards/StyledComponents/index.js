bed9222da42c11d250634ac2c603ad4ebe4fac8d
erozonachi <erozonachi.james@gmail.com>
2019-07-02 19:00:09 +0100

    Create and export circle styled component

---
+++ src/components/cards/StyledComponents/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Circle = styled.div`
+  -webkit-clip-path: circle(50% at 50% 50%);
+  clip-path: circle(50% at 50% 50%);
+  width: ${props.small? '30px' : '80%'};
+`;
