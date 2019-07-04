cf3588c9497429611a7c1a5ca39ed1ef6287a365
erozonachi <erozonachi.james@gmail.com>
2019-07-04 15:26:21 +0100

    Create and export side_bar_container styled component

--- src/components/commons/StyledComponents/index.js
+++ src/components/commons/StyledComponents/index.js
@@ -41,3 +41,44 @@ export const Nav = styled.div`
     margin: 0 .25em;
   }
 `;
+
+export const SideBarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 60px;
+  left: 0;
+  bottom: 0;
+  background: transparent;
+  color: #200122;
+  font-family: 'El Messiri', sans-serif;
+  z-index: 999;
+  > a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: .5em;
+    color: inherit;
+    text-decoration: none;
+    &:link, &:visited {
+      color: inherit;
+    }
+    > strong {
+      font-weight: bold;
+      display: none;
+    }
+  }
+  &:hover {
+    background: #200122;
+    background: -webkit-linear-gradient(to right, #6f0000, #200122);
+    background: linear-gradient(to right, #6f0000, #200122);
+    color: #ddd;
+    > a {
+      > strong {
+        display: inline;
+      }
+    }
+  }
+`;
